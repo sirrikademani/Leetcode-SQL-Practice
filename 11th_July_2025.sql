a54f1bc9bb80c3fed785ac31255c30164008b95a
Siri Kademani <120608084+sirrikademani@users.noreply.github.com>
2025-07-11 12:12:52 -0700

    Update 11th_July_2025.sql

--- 11th_July_2025.sql
+++ 11th_July_2025.sql
@@ -55,3 +55,39 @@ and d.customer_id=f.customer_id
 select 
 round(sum(case when delivery_type='immediate' then 1 else 0 end)/count(delivery_id)*100,2) as immediate_percentage
 from cte;
+
+/* 4. 550. Game Play Analysis IV
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+SQL Schema
+Pandas Schema
+Table: Activity
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| player_id    | int     |
+| device_id    | int     |
+| event_date   | date    |
+| games_played | int     |
++--------------+---------+
+(player_id, event_date) is the primary key (combination of columns with unique values) of this table.
+This table shows the activity of players of some games.
+Each row is a record of a player who logged in and played a number of games (possibly 0) before logging out on someday using some device.
+
+Write a solution to report the fraction of players that logged in again on the day after the day they first logged in, rounded to 2 decimal places. In other words, you need to determine the number of players who logged in on the day immediately following their initial login, and divide it by the number of total players.
+
+The result format is in the following example.*/
+
+select
+round(count(distinct player_id)/(select count(distinct player_id) from activity),2) as fraction
+from Activity 
+where (player_id,date_sub(event_date, INTERVAL 1 day))
+in (select player_id, min(event_date) as first_login
+from activity group by player_id);
+
+
+
