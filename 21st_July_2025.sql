f464ee5c8e73631ea832cab31c65b9295e850160
Siri Kademani <120608084+sirrikademani@users.noreply.github.com>
2025-07-21 23:30:08 -0700

    Update 21st_July_2025.sql

--- 21st_July_2025.sql
+++ 21st_July_2025.sql
@@ -21,15 +21,33 @@ group by 1,2 having count(distinct i1.user_id)>=3
 order by 3 desc,1 asc,2 asc;
 
 
-select lb.book_id,
-lb.title,
-lb.author,
-lb.genre,
-lb.publication_year,
-count(lb.record_id) as current_borrowers
-from cte lb
-where total_copies=0
-group by 1,2,3,4,5
-order by count(lb.record_id) desc,title asc
+--2.https://leetcode.com/problems/first-letter-capitalization-ii/description/
+with recursive temp as(
+    select content_id, length(content_text) as lens, content_text
+    from user_content
+)
 
+,temp2 as (
+    select content_id, lens, 1 as id,  content_text, substring(content_text,1,1) as curr_letter
+     from temp
+     union all
+     select content_id, lens, id+1,  content_text, substring(content_text,id+1,1) as curr_letter
+     from temp2 where id <= lens
+)
+
+,temp3 as (select content_id, lens, id,  content_text, curr_letter ,
+lag(curr_letter,1,' ') over (partition by content_id order by id) as prev_char 
+from temp2)
+
+,temp4 as (select content_id, lens, id,  content_text, curr_letter ,prev_char,
+(case when prev_char=" " or (prev_char="-" and curr_letter between "a" and "z") then upper(curr_letter)
+when curr_letter between "A" and "Z" then lower(curr_letter) 
+else curr_letter
+end) as next
+from temp3)
+
+select content_id, content_text as original_text,
+group_concat(next order by id separator '') as converted_text from temp4
+group by content_id
+;
 
