4be9fe6b4657fbf68dca7aff7d07f03f59c65d62
Siri Kademani <120608084+sirrikademani@users.noreply.github.com>
2025-07-28 13:28:29 -0700

    Update 28th_July_2025_DataLemur.sql

--- 28th_July_2025_DataLemur.sql
+++ 28th_July_2025_DataLemur.sql
@@ -19,3 +19,35 @@ FROM transactions)
 
 select user_id,spend,transaction_date
 from cte where third_transaction_no=3 ;
+
+--4. https://datalemur.com/questions/sql-second-highest-salary
+/* Second Highest Salary*/
+--Method1:
+SELECT max(salary) as second_highest_salary
+FROM employee
+where salary<(select max(salary) from employee);
+
+--Method2:
+with cte as (SELECT *,
+dense_rank() over (order by salary desc) as rn
+FROM employee
+)
+
+select distinct salary from cte where rn=2;
+
+--5.https://datalemur.com/questions/supercloud-customer
+WITH supercloud_cust AS (
+  SELECT 
+    customers.customer_id, 
+    COUNT(DISTINCT products.product_category) AS product_count
+  FROM customer_contracts AS customers
+  INNER JOIN products 
+    ON customers.product_id = products.product_id
+  GROUP BY customers.customer_id
+)
+
+SELECT customer_id
+FROM supercloud_cust
+WHERE product_count = (
+  SELECT COUNT(DISTINCT product_category) FROM products
+);
