5b46b7eb1d3f7fbdc2d175040eecbd91909bbe6a
Siri Kademani <120608084+sirrikademani@users.noreply.github.com>
2025-07-29 12:20:03 -0700

    Update 29th_July_2025_DL.sql

--- 29th_July_2025_DL.sql
+++ 29th_July_2025_DL.sql
@@ -10,3 +10,22 @@ SELECT
 sum(case when device_type ='laptop' then 1 else 0 end) as laptop_reviews,
 sum(case when device_type in ('phone','tablet') then 1 else 0 end) as mobile_views
 FROM viewership;
+
+--2.https://datalemur.com/questions/time-spent-snaps
+SELECT 
+ab.age_bucket,
+round((sum(case when activity_type='open' then time_spent end)/sum(case when activity_type in ('send','open') then time_spent end))*100.0,2) as open_perc,
+round((sum(case when activity_type='send' then time_spent end)/sum(case when activity_type in ('send','open') then time_spent end))*100.0,2) as send_perc
+FROM activities a 
+left join age_breakdown ab on a.user_id=ab.user_id
+group by age_bucket order by age_bucket
+;
+
+--3.https://datalemur.com/questions/rolling-average-tweets
+SELECT
+user_id,
+tweet_date,
+round(avg(tweet_count) over (partition by user_id order by tweet_date rows between 2 PRECEDING and CURRENT row),2)
+as rolling_avg_3rd
+from tweets
+;
